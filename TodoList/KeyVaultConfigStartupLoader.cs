6a3378a312839f9ee150b277ac64678e0b0f589d
cawa <cawa@MININT-ILSQLBT>
2017-04-28 12:57:41 -0700

    update project

---
+++ TodoList/KeyVaultConfigStartupLoader.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using Microsoft.AspNetCore.Hosting;
+[assembly: HostingStartup(typeof(Microsoft.AspNetCore.ApplicationInsights.HostingStartup.KeyvaultConfigStartupLoader))]
+namespace Microsoft.AspNetCore.ApplicationInsights.HostingStartup
+{
+    /// <summary>
+    /// A dynamic Application Insights lightup experiance
+    /// </summary>
+    public class KeyvaultConfigStartupLoader : IHostingStartup
+    {
+        /// <summary>
+        /// Calls UseApplicationInsights
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.AddAzureKeyVault(
+           $"https://{config["Vault"]}.vault.azure.net/");
+
+            Configuration = builder.Build();
+
+        }
+    }
+}
